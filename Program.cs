c00f2ebcd43b49c6804c75590ae456b6d1cf68f9
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-04-14 10:28:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonacniAutomatBudicMarin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        SlucajA:
+            
+            if (Console.ReadKey().KeyChar == '1') {  }
+            else { goto SlucajA; }
+            
+            
+            if (Console.ReadKey().KeyChar == '1') {  }
+            else { goto SlucajA; }
+            SlucajC:
+            
+            if (Console.ReadKey().KeyChar == '0') {  }
+            else { goto SlucajC; }
+            if (Console.ReadKey().KeyChar == '1')
+            {
+                Console.WriteLine("Dizi rampu!!!");
+            }
+            else { goto SlucajA; }
+            Console.ReadKey();
+
+        }
+    }
+}
